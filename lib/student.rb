d315ff8564f35990b35c03361a25e63d444c0b90
lmonty22 <lindsay.montgomery@olympian.org>
2020-03-18 12:20:32 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,8 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT MAX(id) FROM students")
+    # @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   def self.create(name:, grade:)
